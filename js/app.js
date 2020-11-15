6f6e616e86dc9f1358b7b43bb8287566de543b99
0ArchLinux0 <blueblueriz1646@daum.net>
2020-11-16 07:03:24 +0900

    .

--- js/app.js
+++ js/app.js
@@ -1,69 +1,80 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 15);
 
-   
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 15);
+/*PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
+fov — Camera frustum vertical field of view. degrees
+aspect — Camera frustum aspect ratio. 
+near — Camera frustum near plane.
+far — Camera frustum far plane.*/
 
-    /*PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
-    fov — Camera frustum vertical field of view. degrees
-    aspect — Camera frustum aspect ratio. 
-    near — Camera frustum near plane.
-    far — Camera frustum far plane.*/
+const canvas = document.querySelector('#canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let prevWidth = canvas.width;
+let prevHeight = canvas.height;
 
-    const canvas=document.querySelector('#c');  
-    canvas.width=window.innerWidth;
-    let prevWidth=canvas.width;
-    canvas.height=window.innerHeight
-    const renderer = new THREE.WebGLRenderer({canvas}); 
-    renderer.setSize(window.innerWidth, window.innerHeight); //setting drawing buffersize
-    //dirrent to canvas size setting in css
-    document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight); //setting drawing buffersize
+//dirrent to canvas size setting in css
+document.body.appendChild(renderer.domElement);
 
 
-    const boxWidth=1;
-    const boxHeight=1;
-    const boxDepth=1;
-    const geometry = new THREE.BoxGeometry(boxWidth,boxHeight,boxDepth); //object contains all the verticles and faces of the cube.
-    const materialNotAffectedByLights = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); //material to color it(+extra)
-    const materialAfftectedByLights=new THREE.MeshPhongMaterial( {color:0x00ff00});
-    const cube = new THREE.Mesh(geometry, materialAfftectedByLights); //takes geometry and applies a material merging
-   	const cubes=[makeInstanceCube("red",2),
-   				 makeInstanceCube("blue",-2),
-   				 ];
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth); //object contains all the verticles and faces of the cube.
+const materialNotAffectedByLights = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); //material to color it(+extra)
+const materialAfftectedByLights = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, materialAfftectedByLights); //takes geometry and applies a material merging
+const cubes = [makeInstanceCube("red", 2),
+    makeInstanceCube("blue", -2),
+];
 
-    scene.add(cube); //add to the coordinates(0,0,0);
+scene.add(cube); //add to the coordinates(0,0,0);
 
-    camera.position.z = 5;
+camera.position.z = 5;
 
-   {
-  const color = 0xFFFFFF;
-  const intensity = 1;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
+{
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
 }
 
-    function animate(t) {
-        if(prevWidth!==window.innerWidth){
-            canvas.width=window.innerWidth;
-            canvas.height=window.innerHeight 
-            renderer.setSize(window.innerWidth, window.innerHeight);
-        }
-        requestAnimationFrame(animate);
-        cube.rotation.x+=0.01;
-        cube.rotation.y+=0.01;
-        renderer.render(scene, camera);
-        cubes.forEach((cube,index)=>{
-        	cube.rotation.x+=-0.01;
-        	cube.rotation.y+=0.01;
-        	console.log(index);
-        })
+function animate(t) {
+    requestAnimationFrame(animate);
+    const pixelRatio = window.devicePixelRatio;
+    console.log(pixelRatio);
+    canvas.width = window.innerWidth * pixelRatio;
+    canvas.height = window.innerHeight * pixelRatio;
+    if ((prevWidth !== canvas.width) || (prevHeight !== canvas.hegith)) { //size change
+        canvas.width = window.innerWidth * pixelRatio;
+        canvas.height = window.innerHeight * pixelRatio; //change canvas size
+        prevWidth = canvas.width;
+        prevHeight = canvas.height; //store prev value to compare
+        renderer.setSize(window.innerWidth, window.innerHeight); //change render size
+        // setting camera aspect to prevent view from crushing
+        camera.aspect = canvas.width / canvas.height;
+        camera.updateProjectionMatrix();
+        //
     }
 
-    function makeInstanceCube(color,x){
-    	const materialNotAffectedByLights=new THREE.MeshPhongMaterial({color});
-    	const cube=new THREE.Mesh(geometry,materialNotAffectedByLights)
-    	cube.position.x=x;
-    	scene.add(cube);
-    	return cube;
-    }
-    animate();
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+    cubes.forEach((cube, index) => {
+        cube.rotation.x += -0.01;
+        cube.rotation.y += 0.01;
+    })
+
+}
+
+function makeInstanceCube(color, x) {
+    const materialNotAffectedByLights = new THREE.MeshPhongMaterial({ color });
+    const cube = new THREE.Mesh(geometry, materialNotAffectedByLights)
+    cube.position.x = x;
+    scene.add(cube);
+    return cube;
+}
+animate();
---
+++ js/app2.js
@@ -0,0 +1,89 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 50);
+camera.position.set(0, 30, 0);
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
+
+const canvas = document.querySelector('#canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let prevWidth = canvas.width;
+let prevHeight = canvas.height;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(canvas.widht, canvas.height);
+document.body.appendChild(renderer.domElement)
+
+const objects = [];
+
+const solarSystem= new THREE.Object3D();
+scene.add(solarSystem);
+objects.push(solarSystem)
+
+const radius = 1;
+const widthSegment = 15;
+const heightSegment = 15;
+const sphereGeometry = new THREE.SphereGeometry(radius, widthSegment, heightSegment);
+const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
+//emissive property is basically the color that will be drawn with no light hitting the surface. Light is added to that color.
+const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+sunMesh.scale.set(5, 5, 5);
+solarSystem.add(sunMesh);
+//#### set with line 34 scene.add(sunMesh);
+
+const earthOrbit= new THREE.Object3D();
+earthOrbit.position.x=10;
+solarSystem.add(earthOrbit);
+objects.push(earthOrbit);
+
+const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
+const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+//scene.add(earthMesh);
+//####set with line 27 sunMesh.add(earthMesh);
+earthOrbit.add(earthMesh);
+objects.push(earthMesh)
+
+const moonOrbit=new THREE.Object3D();
+moonOrbit.position.x=2;
+earthOrbit.add(moonOrbit);
+objects.push(moonOrbit);
+
+const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
+const moonMesh=new THREE.Mesh(sphereGeometry, moonMaterial);
+moonMesh.scale.set(.5,.5,.5);
+moonOrbit.add(moonMesh);
+objects.push(moonMesh)
+
+//Light
+{
+    const color = 0xFFFFFF;
+    const intensity = 3;
+    const light = new THREE.PointLight(color, intensity);
+    scene.add(light);
+}
+
+
+function animate(time) {
+    requestAnimationFrame(animate);
+    const pixelRatio = window.devicePixelRatio;
+    console.log(pixelRatio);
+    canvas.width = window.innerWidth * pixelRatio;
+    canvas.height = window.innerHeight * pixelRatio;
+    if ((prevWidth !== canvas.width) || (prevHeight !== canvas.hegith)) { //size change
+        canvas.width = window.innerWidth * pixelRatio;
+        canvas.height = window.innerHeight * pixelRatio; //change canvas size
+        prevWidth = canvas.width;
+        prevHeight = canvas.height; //store prev value to compare
+        renderer.setSize(window.innerWidth, window.innerHeight); //change render size
+        // setting camera aspect to prevent view from crushing
+        camera.aspect = canvas.width / canvas.height;
+        camera.updateProjectionMatrix();
+        //
+    }
+    renderer.render(scene,camera)
+    objects.forEach((obj) => {
+    obj.rotation.y = time/800;
+    })
+
+}
+    animate();
